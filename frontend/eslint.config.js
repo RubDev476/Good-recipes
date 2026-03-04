7c25425c651a8b39201081203f9ae5fa9e112bf4
ruben-palma <ruben.palma@dineroinmediato.com.mx>
2026-03-04 11:15:39 -0600

    eslint react hook uninstalled

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,12 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
+//import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig([
+  //reactHooks.configs.flat.recommended,
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -14,15 +16,15 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
+      //'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      //...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-)
+])
